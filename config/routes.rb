d4e79cecc356229774d9dc7dbc072b458841f820
awal <ameymwalimbe@gmail.com>
2024-10-27 16:38:51 -0700

    CRUD for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root "tweets#index"
+  root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
 
   get "/logout", to: "sessions#destroy"
   delete "/logout", to: "sessions#destroy"
+
+  resources :posts
 end
