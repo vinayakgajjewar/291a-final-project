d05b2824b281c7eaf7c9f7a4cc308c95e7078f0f
awal <ameymwalimbe@gmail.com>
2024-10-27 11:25:15 -0700

    whitespace fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "tweets#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
 
