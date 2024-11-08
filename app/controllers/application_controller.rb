5cd7ce89c9dff1b11b8441d76bfc6a9e28696257
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2024-11-08 10:50:40 -0800

    Run rails new

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
-  helper_method :current_user
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-end
--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-  validates :content, presence: true
-end
